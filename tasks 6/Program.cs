6ef3987aff09b99b24de797bc89c8193f0c472c0
Сергей Кашмель <107143266+SiarheiKashmel@users.noreply.github.com>
2022-07-26 18:19:08 +0400

    Добавьте файлы проекта.

---
+++ tasks 6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var count = (new DateTime(DateTime.Now.Year, 12, 31) - DateTime.Now).Days;
+            Console.WriteLine("Количество дней до нового года: {0} ", count);
+        }
+    }
+}
